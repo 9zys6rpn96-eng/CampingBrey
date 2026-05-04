34c882e538f7de4329f5a7dc0f12733cae114360
Johannes Eich <johannes@familie-eich.de>
2026-05-04 11:37:13 +0200

    route app under buchungssystem

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPage } from "./LandingPage";
 import AdminApp from "./AdminApp";
+
+function EmptyPage() {
+  return <div />;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<AdminApp />} />
+        <Route path="/" element={<EmptyPage />} />
+        <Route path="/buchungssystem/*" element={<AdminApp />} />
       </Routes>
     </BrowserRouter>
   );
